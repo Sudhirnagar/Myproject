df2f7bd77366a9a6ece0df34671cc842f0b7e123
sudhi <sunil9636990666@gmail.com>
2025-07-29 16:02:30 +0530

    final update

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,31 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:zingchat/widgets/chat_messages.dart';
 import 'package:zingchat/widgets/new_messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setPushNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
